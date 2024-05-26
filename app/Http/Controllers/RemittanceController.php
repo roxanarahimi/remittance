168340f9b79629d722eb614b76e455f0ee944d2f
Roxana <ms.roxanarahimi@gmail.com>
2024-05-26 22:22:36 +0330

    store

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -61,7 +61,7 @@ class RemittanceController extends Controller
         if ($validator->fails()) {
             return response()->json($validator->messages(), 422);
         }
-        return $request->OrderItems;
+        return gettype($request->OrderItems);
         try {
             foreach ($request->OrderItems as $item ){
                 Remittance::create([

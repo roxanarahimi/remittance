fb29d40b437e0fb47bbbe3858372a4ad77daff0a
Roxana <ms.roxanarahimi@gmail.com>
2024-05-28 01:54:25 +0330

    store

--- config/app.php
+++ config/app.php
@@ -185,6 +185,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        'Redis' => Illuminate\Support\Facades\Redis::class,
     ])->toArray(),
 
 ];

20faccba748e6104c3d321e1c3339defda76c3b5
Roxana <ms.roxanarahimi@gmail.com>
2024-05-28 01:47:02 +0330

    store

--- config/database.php
+++ config/database.php
@@ -141,8 +141,8 @@ return [
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
-            'serializer' => Redis::SERIALIZER_MSGPACK,
-            'compression' => Redis::COMPRESSION_LZ4
+//            'serializer' => Redis::SERIALIZER_MSGPACK,
+//            'compression' => Redis::COMPRESSION_LZ4
             ],
 
         'default' => [

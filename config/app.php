2601568e1f8d4ec65c663ba86982f0d728e2116f
Roxana <ms.roxanarahimi@gmail.com>
2024-05-28 01:50:22 +0330

    store

--- config/app.php
+++ config/app.php
@@ -168,6 +168,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Illuminate\Redis\RedisServiceProvider::class,
+
     ])->toArray(),
 
     /*

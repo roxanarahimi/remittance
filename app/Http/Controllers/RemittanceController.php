2dd329b73daad07d9af923684335e05730061ab1
Roxana <ms.roxanarahimi@gmail.com>
2024-05-28 04:29:32 +0330

    store

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -66,11 +66,11 @@ class RemittanceController extends Controller
 
         //return $request;
 
-         $redis = Redis::connect('127.0.0.1',3306);
+//         $redis = Redis::connect('127.0.0.1',3306);
 //        $redis = new Redis();
 //        $redis->connect('127.0.0.1', 6379);
 
-        $x = $redis->set('aaa','bbb');
+        $x = Redis::set('aaa','bbb');
          return $x;
          $r = Redis::get('aaa');
          return $r;

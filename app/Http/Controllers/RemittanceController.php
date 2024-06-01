dbd48aad08e47b1db509ad71d17c38920928a3ca
Roxana <ms.roxanarahimi@gmail.com>
2024-06-02 02:25:37 +0330

    store

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -321,7 +321,7 @@ class RemittanceController extends Controller
                 ->whereDoesntHave('Store.Plant.Address', function ($q) {
                     $q->where('Details', 'LIKE', '%گرمدره%');
                 })
-                ->get(100);
+                ->take(100)->get();
             return InventoryVoucherResource::collection($x);
             $dat = DB::connection('sqlsrv')->table('LGS3.InventoryVoucher')//InventoryVoucherItem//InventoryVoucherItemTrackingFactor//Part//Plant//Store
             ->join('LGS3.Store', 'LGS3.Store.StoreID', '=', 'LGS3.InventoryVoucher.CounterpartStoreRef')

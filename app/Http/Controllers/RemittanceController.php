9bf7e533ea3d939f731316c89cc62b7afe6d4713
Roxana <ms.roxanarahimi@gmail.com>
2024-06-02 03:07:19 +0330

    store

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -321,11 +321,11 @@ class RemittanceController extends Controller
                     $q->where('Details','LIKE','%گرمدره%');
                 })
                 ->with('OrderItems')
-                ->paginate(100)->toArray();
+                ->paginate(100);
 
 
-            $x['data'] = InventoryVoucherResource::collection($x['data']);
-            return $x;
+//            $x['data'] = InventoryVoucherResource::collection($x['data']);
+            return $x->data;
             $dat = DB::connection('sqlsrv')->table('LGS3.InventoryVoucher')//InventoryVoucherItem//InventoryVoucherItemTrackingFactor//Part//Plant//Store
             ->join('LGS3.Store', 'LGS3.Store.StoreID', '=', 'LGS3.InventoryVoucher.CounterpartStoreRef')
                 ->join('LGS3.Plant', 'LGS3.Plant.PlantID', '=', 'LGS3.Store.PlantRef')

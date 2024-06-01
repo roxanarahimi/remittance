1e6e002a07ac91eba4ff96ce58df36a75b2ba128
Roxana <ms.roxanarahimi@gmail.com>
2024-06-02 00:45:26 +0330

    store

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -307,8 +307,8 @@ class RemittanceController extends Controller
                 ->orWhere('InventoryVoucherSpecificationRef', '=', 69)//68, 69
                 ->where('FiscalYearRef', 1403)
                 ->take(200)->get();
-            $l = InventoryVoucherResource::collection($x)->whenHas('ok',1);
-            return \response(InventoryVoucherResource::collection($x));
+            $l = InventoryVoucherResource::collection($x)->withQuery(['ok','>',0]);
+            return \response($l);
             $dat = DB::connection('sqlsrv')->table('LGS3.InventoryVoucher')//InventoryVoucherItem//InventoryVoucherItemTrackingFactor//Part//Plant//Store
             ->join('LGS3.Store', 'LGS3.Store.StoreID', '=', 'LGS3.InventoryVoucher.CounterpartStoreRef')
                 ->join('LGS3.Plant', 'LGS3.Plant.PlantID', '=', 'LGS3.Store.PlantRef')

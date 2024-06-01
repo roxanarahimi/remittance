e26234431ae9bb4c43d4e24c7a6656d3da38fd91
Roxana <ms.roxanarahimi@gmail.com>
2024-06-02 01:02:52 +0330

    store

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -309,7 +309,7 @@ class RemittanceController extends Controller
                 ->where('InventoryVoucherSpecificationRef', '=', 68)//68, 69
                 ->orWhere('InventoryVoucherSpecificationRef', '=', 69)//68, 69
                 ->where('FiscalYearRef', 1403)
-                ->with('OrderItems')->where('Name','LIKE','%نودالیت%')
+                ->with('InventoryVoucherItem')->where('Name','LIKE','%نودالیت%')
                 ->take(200)->get();
             $l = InventoryVoucherResource::collection($x)->withQuery(['ok','>',0]);
             return \response($l);

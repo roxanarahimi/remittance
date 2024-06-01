db98f482c5754ec644798c3291f3bf237e86f343
Roxana <ms.roxanarahimi@gmail.com>
2024-06-02 00:35:31 +0330

    store

--- app/Http/Resources/InventoryVoucherItemResource.php
+++ app/Http/Resources/InventoryVoucherItemResource.php
@@ -20,7 +20,7 @@ class InventoryVoucherItemResource extends JsonResource
             "ProductName" => $this->Part->Name,
             "ProductNumber" => $this->Part->Code,
             "Quantity" => $this->Quantity,
-            "NPart" => $this->NPart,
+            "NPart" => $this->NPart?->Name,
 
         ];
     }

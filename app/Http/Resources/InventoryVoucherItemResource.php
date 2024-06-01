8551e541a214b1949e3af4300be8b4a0e3c38d7d
Roxana <ms.roxanarahimi@gmail.com>
2024-06-01 19:40:34 +0330

    store

--- app/Http/Resources/InventoryVoucherItemResource.php
+++ app/Http/Resources/InventoryVoucherItemResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class InventoryVoucherResource extends JsonResource
+class InventoryVoucherItemResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -15,10 +15,7 @@ class InventoryVoucherResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            "orderID" => $this->InventoryVoucherID,
-            "Number" => $this->Number,
-            "OrderItems" => InventoryVoucherItemResource::collection($this->OrderItems),
-
+            "InventoryVoucherItemID" => $this->InventoryVoucherItemID,
         ];
     }
 }

08d090e060d2d182bbcdfceaa6dffcd72bebcbea
Roxana <ms.roxanarahimi@gmail.com>
2024-06-02 07:38:55 +0330

    store

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -317,7 +317,7 @@ class RemittanceController extends Controller
                 ->orWhere('LGS3.InventoryVoucher.InventoryVoucherSpecificationRef', '=', 69)//68, 69
                 ->where('LGS3.InventoryVoucher.FiscalYearRef', 1403)
                 ->where('LGS3.InventoryVoucher.CounterpartStoreRef')
-                ->wereHas('OrderItems', function ($query) {
+                ->with('OrderItems', function ($query) {
                     $query->OrderItems()->Parts()->where('Name', 'like', '%نودالیت%');
                         })
 

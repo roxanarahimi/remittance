66bbe2a4b9644d9510f6492e78a74d2c8e092200
Roxana <ms.roxanarahimi@gmail.com>
2024-06-02 05:55:38 +0330

    store

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -309,25 +309,17 @@ class RemittanceController extends Controller
                 ->join('LGS3.Store', 'LGS3.Store.StoreID', '=', 'LGS3.InventoryVoucher.CounterpartStoreRef')
                 ->join('LGS3.Plant', 'LGS3.Plant.PlantID', '=', 'LGS3.Store.PlantRef')
                 ->join('GNR3.Address', 'GNR3.Address.AddressID', '=', 'LGS3.Plant.AddressRef')
+
                 ->whereNot('LGS3.Store.Name', 'LIKE', "%گرمدره%")//68, 69
                 ->whereNot('GNR3.Address.Details', 'LIKE', "%گرمدره%")//68, 69
-                ->where('InventoryVoucherSpecificationRef', '=', 68)//68, 69
-                ->orWhere('InventoryVoucherSpecificationRef', '=', 69)//68, 69
-                ->where('FiscalYearRef', 1403)
-                ->where('LGS3.InventoryVoucher.CounterpartStoreRef')
+                ->where('LGS3.InventoryVoucher.InventoryVoucherSpecificationRef', '=', 68)//68, 69
+                ->orWhere('LGS3.InventoryVoucher.InventoryVoucherSpecificationRef', '=', 69)//68, 69
+                ->where('LGS3.InventoryVoucher.FiscalYearRef', 1403)
+                ->orderByDesc('LGS3.InventoryVoucher.InventoryVoucherID')
                 ->whereHas('OrderItems.Part', function($q){
                     $q->where('Name','LIKE','%نودالیت%');
                 })
-//                ->whereJsonContains('OrderItems->Part->Name', ['نودالیت'])
-
-//                ->whereDoesntHave('Store', function($q){
-//                    $q->where('Name','LIKE','%گرمدره%');
-//                })
-//                ->whereDoesntHave('Store.Plant.Address', function($q){
-//                    $q->where('Details','LIKE','%گرمدره%');
-//                })
-//                ->with('OrderItems')
-                ->get()->toArray();
+                ->get()->unique()->toArray();
 
 //            return $x;
 

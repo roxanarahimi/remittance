c0d56ab865e95c3ac22085466062e91e12e47686
Roxana <ms.roxanarahimi@gmail.com>
2024-06-02 04:36:10 +0330

    store

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -342,7 +342,7 @@ class RemittanceController extends Controller
                 $offset = ($request['page'] - 1) * $perPage;
             }
             $info = array_slice($input, $offset, $perPage);
-            $result =json_encode($info[0]);
+            $result =json_decode($info);
 
             return $result;
             $obj = json_decode($result);

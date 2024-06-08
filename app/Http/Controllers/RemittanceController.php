a8bb5763c065ccce612401bc206b592b636303f1
Roxana <ms.roxanarahimi@gmail.com>
2024-06-08 08:38:05 +0330

    filterZ

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -317,14 +317,18 @@ class RemittanceController extends Controller
                 ->orWhere('LGS3.InventoryVoucher.InventoryVoucherSpecificationRef', '=', 69)//68, 69
                 ->where('LGS3.InventoryVoucher.FiscalYearRef', 1403)
                 ->where('LGS3.InventoryVoucher.CounterpartStoreRef')
-                ->with(['OrderItems' => function($q) {
-                    $q->whereHas('Part', function($query) {
-                        $query->where('Name', 'like', '%نودالیت%');
-                    });
-                }])
-                ->whereHas('OrderItems.Part', function($query) {
-                    $query->where('Name', 'like', '%نودالیت%');
-                })
+//                ->with(['OrderItems' => function($q) {
+//                    $q->whereHas('Part', function($query) {
+//                        $query->where('Name', 'like', '%نودالیت%');
+//                    });
+//                }])
+//                ->whereHas('OrderItems.Part', function($query) {
+//                    $query->where('Name', 'like', '%نودالیت%');
+//                })
+//                    ->where()->OkItems(true)
+                    ->whereHas('OkItems',function($q){
+                        $q = true;
+                    })
                 ->orderByDesc('LGS3.InventoryVoucher.InventoryVoucherID')
                 ->take(100)->get();
 //            $x = InventoryVoucherResource::collection($x);
--- app/Models/InventoryVoucher.php
+++ app/Models/InventoryVoucher.php
@@ -16,6 +16,17 @@ class InventoryVoucher extends Model
     {
         return $this->hasMany(InventoryVoucherItem::class,  'InventoryVoucherRef','InventoryVoucherID');
     }
+    public function OkItems()
+    {
+        $x= $this->hasMany(InventoryVoucherItem::class,  'InventoryVoucherRef','InventoryVoucherID');
+        foreach ($x as $item){
+            if(str_contains($item->Part->Name,'نودالیت')){
+                return true;
+            }else{
+                return false;
+            }
+        }
+    }
     public function Store()
     {
         return $this->belongsTo(Store::class, 'CounterpartStoreRef','StoreID');

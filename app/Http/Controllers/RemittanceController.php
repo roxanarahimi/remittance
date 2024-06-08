4cd45dbc9c861d5d417b44ec30a18f4d6a42c671
Roxana <ms.roxanarahimi@gmail.com>
2024-06-08 10:08:29 +0330

    filterZ

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -321,8 +321,8 @@ class RemittanceController extends Controller
                 ->orWhere('LGS3.InventoryVoucher.InventoryVoucherSpecificationRef', '=', 69)//68, 69
                 ->where('LGS3.InventoryVoucher.FiscalYearRef', 1403)
                 ->where('LGS3.InventoryVoucher.CounterpartStoreRef')
-                ->whereHas('OrderItems',function ($q) use ($ids){
-                    $q->whereIn('PartRef', $ids);
+                ->whereHas('OkItems',function ($q){
+                    $q->whereNotNull('PartRef');
                 })
                 ->with('OkItems')
                 ->orderByDesc('LGS3.InventoryVoucher.InventoryVoucherID')

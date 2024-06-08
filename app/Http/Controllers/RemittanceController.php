4e41053fcc19d70a1b68b78342b83f99094fc231
Roxana <ms.roxanarahimi@gmail.com>
2024-06-08 10:34:30 +0330

    filterZ

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -322,7 +322,7 @@ class RemittanceController extends Controller
                 ->where('LGS3.InventoryVoucher.FiscalYearRef', 1403)
                 ->where('LGS3.InventoryVoucher.CounterpartStoreRef')
                 ->whereHas('OkItems',function($q){
-                    $q != null;
+                    $q->withCount();
                 })
                 ->with('OkItems')
                 ->orderBy('LGS3.InventoryVoucher.InventoryVoucherID','DESC')

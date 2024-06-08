52144caac99a5953fed3276d6d7d69792be3cf73
Roxana <ms.roxanarahimi@gmail.com>
2024-06-08 10:45:08 +0330

    filterZ

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -324,7 +324,7 @@ class RemittanceController extends Controller
                 ->whereHas('OkItems')
                 ->with('OkItems')
                 ->orderBy('LGS3.InventoryVoucher.InventoryVoucherID','DESC')
-                ->take(100)->get();
+                ->take(100)->get()->where('ok_items');
 
             return response()->json($x, 200);
             return response()->json(InventoryVoucherResource::collection($x), 200);

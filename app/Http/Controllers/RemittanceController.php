e5a06a2a1ca536dcdb40659dc322d3a63e85d895
Roxana <ms.roxanarahimi@gmail.com>
2024-06-09 19:40:44 +0330

    stores

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -331,7 +331,7 @@ class RemittanceController extends Controller
                 ->join('LGS3.Plant', 'LGS3.Plant.PlantID', '=', 'LGS3.Store.PlantRef')
                 ->join('GNR3.Address', 'GNR3.Address.AddressID', '=', 'LGS3.Plant.AddressRef')
                 ->where('LGS3.InventoryVoucher.FiscalYearRef', 1403)
-//                ->where('LGS3.InventoryVoucher.CounterpartStoreRef', $request['id'])
+                ->where('LGS3.InventoryVoucher.CounterpartStoreRef', $request['id'])
                 ->whereHas('OrderItems', function ($query) {
                     $query->whereHas('Part', function ($q) {
                         $q->where('Name', 'like', '%نودالیت%');

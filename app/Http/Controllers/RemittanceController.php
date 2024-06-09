f0f062174b6377717a905904900fc8b9f09958df
Roxana <ms.roxanarahimi@gmail.com>
2024-06-09 19:26:02 +0330

    stores

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -342,7 +342,7 @@ class RemittanceController extends Controller
                 ->orderBy('LGS3.InventoryVoucher.InventoryVoucherID', 'DESC')
                 ->paginate(50)->toArray();
 
-            $d gettype((object)$x['data'][0]);
+            $d =(object)$x['data'][0];
             return new InventoryVoucherResource($d);
             $dataa = [];
             foreach ($x['data'] as $item) {

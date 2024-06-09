8a207c7c1f21070fa7b09fc79b91ed0b82acec24
Roxana <ms.roxanarahimi@gmail.com>
2024-06-09 19:44:06 +0330

    stores

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -343,7 +343,7 @@ class RemittanceController extends Controller
                 ->paginate(100);
 
 
-            return InventoryVoucherResource::collection($x['data']);
+            return InventoryVoucherResource::collection($x->{'data'});
             $offset = 0;
             $perPage = 100;
             $t = InventoryVoucherResource::collection($x);

5fa2ea30cd41d48a76385c1622307480fdde4cf0
Roxana <ms.roxanarahimi@gmail.com>
2024-06-09 15:34:57 +0330

    filterZ

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -348,7 +348,7 @@ class RemittanceController extends Controller
                 })
                 ->with('OkItems')
                 ->orderBy('LGS3.InventoryVoucher.InventoryVoucherID','DESC')
-                ->take(100)->get();
+                ->take(2100)->get();
             $t = InventoryVoucherResource::collection($x);
             $tt = array_filter(json_decode($t->toJson(), true), function ($item){
                 return $item['OkItems'] != null;

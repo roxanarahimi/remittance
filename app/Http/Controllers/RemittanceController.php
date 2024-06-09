6fe0260e1c5f947e7674dcf7c2f68f06e4175f92
Roxana <ms.roxanarahimi@gmail.com>
2024-06-09 16:37:50 +0330

    filterZ

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -332,6 +332,7 @@ class RemittanceController extends Controller
                  ->whereNot('LGS3.Store.Name', 'LIKE', "%گرمدره%")
                 ->whereNot('GNR3.Address.Details', 'LIKE', "%گرمدره%")
                 ->where('LGS3.InventoryVoucher.FiscalYearRef', 1403)
+                ->where('LGS3.InventoryVoucher.State', 2)
                 ->where('LGS3.InventoryVoucher.CounterpartStoreRef',$id)
 //                ->whereHas('OrderItems', function ($query) {
 //                    $query->whereHas('Part',function($q){
@@ -352,7 +353,7 @@ class RemittanceController extends Controller
                 })
                 ->with('OrderItems')
                 ->orderBy('LGS3.InventoryVoucher.InventoryVoucherID','DESC')
-                ->take(500)->get();
+                ->get()->count();
 
             return $x;
             $t = InventoryVoucherResource::collection($x);

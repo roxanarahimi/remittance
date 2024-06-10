05dabf7f209189cc736649204967852b1e015742
Roxana <ms.roxanarahimi@gmail.com>
2024-06-10 06:30:46 +0330

    Orders

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -10,7 +10,7 @@ class OrderItem extends Model
     use HasFactory;
 
     protected $connection = 'sqlsrv';
-    protected $table = 'LGS3.InventoryVoucherItem';
+    protected $table = 'SLS3.OrderITem';
     protected $hidden = ['Version'];
 
     public function Order()

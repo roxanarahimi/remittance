d4a91fa3c3e83dac843bffd8573627f3d11bc868
Roxana <ms.roxanarahimi@gmail.com>
2024-06-10 07:26:23 +0330

    Orders

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -349,12 +349,11 @@ class RemittanceController extends Controller
 //                        $q->where('Name', 'like', '%نودالیت%');
 //                    });
 //                })
-//                ->whereHas('OrderItems', function ($q) {
-//                        $q->where(function ($z){
-//                            $z->sum('Quantity') >= 50;
-//                        });
-//                })
-                    ->with('Sum')
+                ->whereHas('OrderItems', function ($q) {
+                        $q->sum('Quantity') >= 50;
+
+                })
+//                    ->with('Sum')
                 ->orderBy('OrderID', 'DESC')
                 ->paginate(20);
 

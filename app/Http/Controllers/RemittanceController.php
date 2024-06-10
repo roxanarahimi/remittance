c003bddc4f8a1e65abb103e39de44af19fb76b61
Roxana <ms.roxanarahimi@gmail.com>
2024-06-10 07:39:45 +0330

    Orders

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -347,7 +347,7 @@ class RemittanceController extends Controller
                 })
 
                 ->orderBy('OrderID', 'DESC')
-                ->unique()->paginate(20);
+                ->paginate(20)->unique();
 
             $data = OrderResource::collection($x);
             return response()->json($x, 200);

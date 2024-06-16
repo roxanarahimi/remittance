52fda00ebd7013cceb6e09d3cdb00f4db47e8674
Roxana <ms.roxanarahimi@gmail.com>
2024-06-16 11:36:14 +0330

    fix

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -189,23 +189,23 @@ class RemittanceController extends Controller
             $last = ceil($x->total() / 100);
             $currentPage = $request['page'] || 1;
             $links = [];
-            $links[] = json_encode( [
-                "url"=> null,
-                "label"=> "&laquo; Previous",
-                "active"=> false
-            ], true);
+            $links[] = {
+                "url": null,
+                "label": "&laquo; Previous",
+                "active": false
+            };
             for($i = 1; $i< $last; $i++){
-                $links[] = json_encode(  [
-                    "url"=> "/?page=".$i,
-                    "label"=> $i,
-                    "active"=> $currentPage === $i
-                ], true);
+                $links[] = {
+                    "url": "/?page=" . $i,
+                    "label": $i,
+                    "active": $currentPage === $i
+                };
             }
-            $links[] = json_encode( [
-                "url"=> "/?page=".$currentPage + 1,
+            $links[] = {
+                "url"=> "/?page=" . $currentPage + 1,
                 "label"=> "Next &raquo;",
                 "active"=> false
-            ], true);
+            };
 //            $links = [
 //        [
 //            "url"=> null,

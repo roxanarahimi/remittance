65510fbc09842933dfcb63cd72fa20b7db58952e
Roxana <ms.roxanarahimi@gmail.com>
2024-06-18 16:36:16 +0330

    wads

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -160,29 +160,29 @@ class RemittanceController extends Controller
     public function readOnly(Request $request)
     {
         try {
-            $x = InventoryVoucher::select("LGS3.InventoryVoucher.InventoryVoucherID", "LGS3.InventoryVoucher.Number",
-                "LGS3.InventoryVoucher.CreationDate", "Date as DeliveryDate", "CounterpartStoreRef")
-                ->join('LGS3.Store', 'LGS3.Store.StoreID', '=', 'LGS3.InventoryVoucher.CounterpartStoreRef')
-                ->join('LGS3.Plant', 'LGS3.Plant.PlantID', '=', 'LGS3.Store.PlantRef')
-                ->join('GNR3.Address', 'GNR3.Address.AddressID', '=', 'LGS3.Plant.AddressRef')
-                ->where('LGS3.InventoryVoucher.FiscalYearRef', 1403)
-//                ->where('LGS3.InventoryVoucher.CounterpartStoreRef', $request['id'])
-                ->whereNot('LGS3.Store.Name', 'LIKE', "%مارکتینگ%")
-                ->whereNot('LGS3.Store.Name', 'LIKE', "%گرمدره%")
-                ->whereNot('GNR3.Address.Details', 'LIKE', "%گرمدره%")
-                ->whereNot('LGS3.Store.Name', 'LIKE', "%ضایعات%")
-                ->whereNot('LGS3.Store.Name', 'LIKE', "%برگشتی%")
-                ->whereHas('OrderItems', function ($query) {
-                    $query->whereHas('Part', function ($q) {
-                        $q->where('Name', 'like', '%نودالیت%');
-                    });
-                })
-                ->where('LGS3.InventoryVoucher.InventoryVoucherSpecificationRef', '=', 68)
-                ->orWhere('LGS3.InventoryVoucher.InventoryVoucherSpecificationRef', '=', 69)
-                ->orderBy('LGS3.InventoryVoucher.InventoryVoucherID', 'DESC')
-                ->paginate(20);
-            $data = InventoryVoucherResource::collection($x);
-            return response()->json($x, 200);
+//            $x = InventoryVoucher::select("LGS3.InventoryVoucher.InventoryVoucherID", "LGS3.InventoryVoucher.Number",
+//                "LGS3.InventoryVoucher.CreationDate", "Date as DeliveryDate", "CounterpartStoreRef")
+//                ->join('LGS3.Store', 'LGS3.Store.StoreID', '=', 'LGS3.InventoryVoucher.CounterpartStoreRef')
+//                ->join('LGS3.Plant', 'LGS3.Plant.PlantID', '=', 'LGS3.Store.PlantRef')
+//                ->join('GNR3.Address', 'GNR3.Address.AddressID', '=', 'LGS3.Plant.AddressRef')
+//                ->where('LGS3.InventoryVoucher.FiscalYearRef', 1403)
+////                ->where('LGS3.InventoryVoucher.CounterpartStoreRef', $request['id'])
+//                ->whereNot('LGS3.Store.Name', 'LIKE', "%مارکتینگ%")
+//                ->whereNot('LGS3.Store.Name', 'LIKE', "%گرمدره%")
+//                ->whereNot('GNR3.Address.Details', 'LIKE', "%گرمدره%")
+//                ->whereNot('LGS3.Store.Name', 'LIKE', "%ضایعات%")
+//                ->whereNot('LGS3.Store.Name', 'LIKE', "%برگشتی%")
+//                ->whereHas('OrderItems', function ($query) {
+//                    $query->whereHas('Part', function ($q) {
+//                        $q->where('Name', 'like', '%نودالیت%');
+//                    });
+//                })
+//                ->where('LGS3.InventoryVoucher.InventoryVoucherSpecificationRef', '=', 68)
+//                ->orWhere('LGS3.InventoryVoucher.InventoryVoucherSpecificationRef', '=', 69)
+//                ->orderBy('LGS3.InventoryVoucher.InventoryVoucherID', 'DESC')
+//                ->paginate(20);
+//            $data = InventoryVoucherResource::collection($x);
+//            return response()->json($x, 200);
 
 //
 

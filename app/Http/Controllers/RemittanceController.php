0435812a4f87b8ddd5393f920152485c2d53552e
Roxana <ms.roxanarahimi@gmail.com>
2024-06-18 16:34:06 +0330

    wads

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -160,6 +160,20 @@ class RemittanceController extends Controller
     public function readOnly(Request $request)
     {
         try {
+            $t = Store::select("LGS3.Store.StoreID", "LGS3.Store.Name as Name", "GNR3.Address.Details")
+                ->join('LGS3.Plant', 'LGS3.Plant.PlantID', '=', 'LGS3.Store.PlantRef')
+                ->join('GNR3.Address', 'GNR3.Address.AddressID', '=', 'LGS3.Plant.AddressRef')
+                ->whereNot('LGS3.Store.Name', 'LIKE', "%مارکتینگ%")
+                ->whereNot('LGS3.Store.Name', 'LIKE', "%گرمدره%")
+                ->whereNot('GNR3.Address.Details', 'LIKE', "%گرمدره%")
+                ->whereNot('LGS3.Store.Name', 'LIKE', "%ضایعات%")
+                ->whereNot('LGS3.Store.Name', 'LIKE', "%برگشتی%");
+            if (isset($request['search'])) {
+                $t = $t->where('LGS3.Store.Name', 'LIKE', "%" . $request['search'] . "%")
+                    ->orWhere('GNR3.Address.Details', 'LIKE', "%" . $request['search'] . "%");
+            }
+            $t = $t->get();
+            return response()->json($t, 200);
 
 /// real place
             $dat = DB::connection('sqlsrv')->table('LGS3.InventoryVoucher')//InventoryVoucherItem//InventoryVoucherItemTrackingFactor//Part//Plant//Store

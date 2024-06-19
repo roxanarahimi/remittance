d72e4856455cc2bb159f66092933e2efd4255eef
Roxana <ms.roxanarahimi@gmail.com>
2024-06-19 09:48:42 +0330

    xt

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -189,7 +189,6 @@ class RemittanceController extends Controller
             foreach ($dat as $item) {
                 $item->{'type'} = 'InventoryVoucher';
                 $item->{'ok'} = 1;
-                $item->{'AddressName'} = $item->{'AddressName'} . substr($item->{'OrderID'}, -3);
                 $item->{'AddressName'} = $item->{'AddressName'} .' '. $item->{'OrderNumber'};
                 $details = DB::connection('sqlsrv')->table('LGS3.InventoryVoucherItem')
                     ->select(["LGS3.Part.Name as ProductName", "LGS3.InventoryVoucherItem.Quantity as Quantity",

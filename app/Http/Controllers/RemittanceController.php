fcfe7de62de95e22fd840b60248b1f75e9ae88ad
Roxana <ms.roxanarahimi@gmail.com>
2024-06-23 23:49:32 +0330

    1Xt2

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -224,13 +224,11 @@ class RemittanceController extends Controller
             $details = DB::connection('sqlsrv')->table('SLS3.OrderItem')
                 ->join('SLS3.Product', 'SLS3.Product.ProductID', '=', 'SLS3.OrderItem.ProductRef')
                 ->select("SLS3.Product.Name as ProductName", "Quantity",
-                    DB::raw('SUM(SLS3.OrderItem.Quantity) as total_amount'),
                     "SLS3.Product.ProductID as Id",
                     "SLS3.Product.Number as ProductNumber"
                 )
                 ->where('OrderRef', $item->{'OrderID'})
                 ->whereIn('SLS3.Product.ProductID', $productIDs)
-                ->havingRaw('SUM(total_amount) > ?', [49])
                 ->get();
             $item->{'OrderItems'} = $details;
 //            foreach ($details as $it) {

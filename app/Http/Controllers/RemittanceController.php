21c1b93c28b0004c4928eb53541dfb5e3a8adcf9
Roxana <ms.roxanarahimi@gmail.com>
2024-06-23 19:24:58 +0330

    1Xt2

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -256,7 +256,7 @@ class RemittanceController extends Controller
                 ->whereHas('OrderItems', function ($q) use ($partIDs) {
                     $q->whereIn('PartRef', $partIDs);
                 })
-                ->whereDate('Date','>=',now()->subDays(7)->startOfDay()->format('d/m/Y'))
+                ->whereDate('CreationDate','>=',now()->subDays(7)->startOfDay()->format('d/m/Y'))
                 ->orderByDesc('LGS3.InventoryVoucher.InventoryVoucherID')
                 ->get();
             return $dat;

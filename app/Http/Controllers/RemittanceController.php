b803b609bdc1ff65d55d169f11dfc1e0bf845673
Roxana <ms.roxanarahimi@gmail.com>
2024-06-23 19:29:22 +0330

    1Xt2

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -258,7 +258,7 @@ class RemittanceController extends Controller
                 })
                 ->where('LGS3.InventoryVoucher.CreationDate','>=',now()->subDays(3)->startOfDay()->format('d/m/Y h:i:s'))
                 ->orderByDesc('LGS3.InventoryVoucher.InventoryVoucherID')
-                ->get();
+                ->paginate(100);
             return $dat;
 
 

d1d4d8a9413bdfb0c2ffe079916e18be55bc782e
Roxana <ms.roxanarahimi@gmail.com>
2024-06-24 08:57:43 +0330

    1Xt2

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -191,6 +191,7 @@ class RemittanceController extends Controller
             ->join('SLS3.Customer', 'SLS3.Customer.CustomerID', '=', 'SLS3.Order.CustomerRef')
             ->join('SLS3.CustomerAddress', 'SLS3.CustomerAddress.CustomerRef', '=', 'SLS3.Customer.CustomerID')
             ->join('GNR3.Address', 'GNR3.Address.AddressID', '=', 'SLS3.CustomerAddress.AddressRef')
+            ->where('SLS3.Order.Date', '>=', today()->subDays(7))
             ->where('SLS3.Order.InventoryRef', 1)
             ->where('SLS3.Order.State', 2)
             ->where('SLS3.Order.FiscalYearRef', 1403)

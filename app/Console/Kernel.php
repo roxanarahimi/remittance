c892f2bc770ed13eac2ad95bdf852901d8e51a28
Roxana <ms.roxanarahimi@gmail.com>
2024-06-25 23:11:20 +0330

    cache...

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('app:cache')
-            ->everyFifteenMinutes()
+            ->everyFiveSeconds()
             ->appendOutputTo('scheduler.log')
             ->timezone('Asia/Tehran');
     }

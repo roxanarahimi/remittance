ae7c3f0adf8658bb9b71c958ad124b15448cab38
Roxana <ms.roxanarahimi@gmail.com>
2024-06-25 22:59:05 +0330

    1Xt2

--- app/Console/Commands/CacheCommand.php
+++ app/Console/Commands/CacheCommand.php
@@ -2,11 +2,15 @@
 
 namespace App\Console\Commands;
 
+use App\Models\InventoryVoucher;
 use App\Models\Invoice;
 use App\Models\InvoiceAddress;
 use App\Models\InvoiceItem;
 use App\Models\InvoiceProduct;
+use App\Models\Order;
+use App\Models\Part;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
 class CacheCommand extends Command
 {
@@ -27,6 +31,62 @@ class CacheCommand extends Command
     /**
      * Execute the console command.
      */
+    public function getInventoryVouchers($inventoryVoucherIDs)
+    {
+        $partIDs = Part::where('Name', 'like', '%نودالیت%')->pluck("PartID");
+        $storeIDs = DB::connection('sqlsrv')->table('LGS3.Store')
+            ->join('LGS3.Plant', 'LGS3.Plant.PlantID', '=', 'LGS3.Store.PlantRef')
+            ->join('GNR3.Address', 'GNR3.Address.AddressID', '=', 'LGS3.Plant.AddressRef')
+            ->whereNot(function ($query) {
+                $query->where('LGS3.Store.Name', 'LIKE', "%مارکتینگ%")
+                    ->orWhere('LGS3.Store.Name', 'LIKE', "%گرمدره%")
+                    ->orWhere('GNR3.Address.Details', 'LIKE', "%گرمدره%")
+                    ->orWhere('LGS3.Store.Name', 'LIKE', "%ضایعات%")
+                    ->orWhere('LGS3.Store.Name', 'LIKE', "%برگشتی%");
+            })
+            ->pluck('StoreID');
+        $dat = InventoryVoucher::select("LGS3.InventoryVoucher.InventoryVoucherID", "LGS3.InventoryVoucher.Number",
+            "LGS3.InventoryVoucher.CreationDate", "Date as DeliveryDate", "CounterpartStoreRef")
+            ->join('LGS3.Store', 'LGS3.Store.StoreID', '=', 'LGS3.InventoryVoucher.CounterpartStoreRef')
+            ->join('LGS3.Plant', 'LGS3.Plant.PlantID', '=', 'LGS3.Store.PlantRef')
+            ->join('GNR3.Address', 'GNR3.Address.AddressID', '=', 'LGS3.Plant.AddressRef')
+//            ->where('LGS3.InventoryVoucher.Date', '>=', today()->subDays(7))
+            ->whereNotIn('LGS3.InventoryVoucher.InventoryVoucherID',$inventoryVoucherIDs)
+            ->whereIn('LGS3.Store.StoreID', $storeIDs)
+            ->where('LGS3.InventoryVoucher.FiscalYearRef', 1403)
+            ->whereIn('LGS3.InventoryVoucher.InventoryVoucherSpecificationRef', [68, 69])
+            ->whereHas('OrderItems', function ($q) use ($partIDs) {
+                $q->whereIn('PartRef', $partIDs);
+            })
+            ->orderByDesc('LGS3.InventoryVoucher.InventoryVoucherID')
+            ->get();
+
+        return $dat;
+    }
+    public function getOrders($orderIDs)
+    {
+        $dat2 = Order::select("SLS3.Order.OrderID", "SLS3.Order.Number",
+            "SLS3.Order.CreationDate", "Date as DeliveryDate", 'SLS3.Order.CustomerRef')
+            ->join('SLS3.Customer', 'SLS3.Customer.CustomerID', '=', 'SLS3.Order.CustomerRef')
+            ->join('SLS3.CustomerAddress', 'SLS3.CustomerAddress.CustomerRef', '=', 'SLS3.Customer.CustomerID')
+            ->join('GNR3.Address', 'GNR3.Address.AddressID', '=', 'SLS3.CustomerAddress.AddressRef')
+//            ->where('SLS3.Order.Date', '>=', today()->subDays(7))
+            ->whereNotIn('SLS3.Order.OrderID',$orderIDs)
+            ->where('SLS3.Order.InventoryRef', 1)
+            ->where('SLS3.Order.State', 2)
+            ->where('SLS3.Order.FiscalYearRef', 1403)
+            ->where('SLS3.CustomerAddress.Type', 2)
+            ->whereHas('OrderItems')
+            ->whereHas('OrderItems', function ($q) {
+                $q->havingRaw('SUM(Quantity) >= ?', [50]);
+            })
+            ->orderBy('OrderID', 'DESC')
+            ->get();
+
+//        $dat2 = OrderResource::collection($dat2);
+        return $dat2;
+    }
+
     public function handle()
     {
 

c103cc6b2f9f477ca76aebda0b49177272783b2c
Roxana <ms.roxanarahimi@gmail.com>
2024-06-25 14:43:08 +0330

    1Xt2

--- app/Http/Controllers/CacheController.php
+++ app/Http/Controllers/CacheController.php
@@ -81,8 +81,8 @@ class CacheController extends Controller
 
     public function cacheInvoice(Request $request): \Illuminate\Http\JsonResponse
     {
-        $inventoryVoucherIDs = Invoice::all()->where('LGS3.InventoryVoucher.Date', '>=', today()->subDays(2))->orderByDesc('id')->pluck('OrderID');
-        $orderIDs = Invoice::all()->where('LGS3.InventoryVoucher.Date', '>=', today()->subDays(2))->orderByDesc('id')->pluck('OrderID');
+        $inventoryVoucherIDs = Invoice::where('LGS3.InventoryVoucher.Date', '>=', today()->subDays(2))->orderByDesc('id')->pluck('OrderID');
+        $orderIDs = Invoice::where('LGS3.InventoryVoucher.Date', '>=', today()->subDays(2))->orderByDesc('id')->pluck('OrderID');
         $d1 = $this->getInventoryVouchers($inventoryVoucherIDs);
         $d2 = $this->getOrders($orderIDs);
 

9f4acdd5cfc47c95c6dfd2b3b759dc84ba89665d
Roxana <ms.roxanarahimi@gmail.com>
2024-06-25 15:00:03 +0330

    1Xt2

--- app/Http/Controllers/CacheController.php
+++ app/Http/Controllers/CacheController.php
@@ -91,7 +91,7 @@ class CacheController extends Controller
 
 
         foreach($d1 as $item){
-            return (array)$item['OrderID'];
+            return $item;
 
             Invoice::create([
                 'Type'=>'InventoryVoucher',

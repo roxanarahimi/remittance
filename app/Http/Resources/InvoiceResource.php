4ddf2db8fc41251f8c768cc85dfd3abcfc574a4b
Roxana <ms.roxanarahimi@gmail.com>
2024-06-25 16:40:24 +0330

    1Xt2

--- app/Http/Resources/InvoiceResource.php
+++ app/Http/Resources/InvoiceResource.php
@@ -16,6 +16,7 @@ class InvoiceResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            "id" => $this->id,
             "OrderID" => $this->OrderID,
             "OrderNumber" => $this->OrderNumber,
 

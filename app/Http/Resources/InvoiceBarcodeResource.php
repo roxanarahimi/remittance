8f03b4881de19351a0fc4e4e896626356a82751b
Roxana <ms.roxanarahimi@gmail.com>
2024-06-26 17:04:54 +0330

    cache...

--- app/Http/Resources/InvoiceBarcodeResource.php
+++ app/Http/Resources/InvoiceBarcodeResource.php
@@ -14,6 +14,13 @@ class InvoiceBarcodeResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            "invoice_item_id" => (int)$this->id,
+            "Id" => $this->invoiceItem->ProductID,
+            "ProductName" => $this->invoiceItem->ProductName,
+            "ProductNumber" => $this->invoiceItem->ProductNumber,
+            "Quantity" => (string)$this->invoiceItem->Quantity,
+            "Barcode" => $this->Barcode
+        ];
     }
 }
--- app/Http/Resources/InvoiceItemResource.php
+++ app/Http/Resources/InvoiceItemResource.php
@@ -23,7 +23,7 @@ class InvoiceItemResource extends JsonResource
             $productNumber = $this->productProduct->ProductNumber;
         }
         return [
-            "invoice_item_id" => $this->id,
+            "invoice_item_id" => (int)$this->id,
             "Id" => $this->ProductID,
             "ProductName" => $productName,
             "ProductNumber" => $productNumber,

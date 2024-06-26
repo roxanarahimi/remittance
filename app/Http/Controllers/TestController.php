12680107bf684211cfeae4d128194fa66a2a4b93
Roxana <ms.roxanarahimi@gmail.com>
2024-06-26 16:50:38 +0330

    cache...

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -40,13 +40,11 @@ class TestController extends Controller
     public function store(Request $request)
     {
 //        return $request;
-        $invoiceItemId = InvoiceItem::where('ProductID', $request['ProductID'])->get();
-        return $invoiceItemId;
-//            ->whereHas('invoice', function ($q) use ($request) {
-//                $q->where('OrderNumber', $request['OrderNumber'])
-//                    ->where('Type', $request['Type']);
-//            })
-//            ->first()->id;
+        $invoiceItemId = InvoiceItem::where('ProductID', $request['ProductID'])
+            ->whereHas('invoice', function ($q) use ($request) {
+                $q->where('OrderNumber', $request['OrderNumber'])->where('Type', $request['Type']);
+            })
+            ->first()->id;
         $myfile = fopen('../storage/logs/failed_data_entries/' . $request['OrderNumber'] . ".log", "w") or die("Unable to open file!");
         $txt = json_encode([
             'OrderNumber' => $request['OrderNumber'],

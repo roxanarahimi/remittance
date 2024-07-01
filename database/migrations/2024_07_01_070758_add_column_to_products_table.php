998314b2a37ed5f92093acd2241aa28805d7d0ae
Roxana <ms.roxanarahimi@gmail.com>
2024-07-01 10:39:16 +0330

    test

--- database/migrations/2024_07_01_070758_add_column_to_products_table.php
+++ database/migrations/2024_07_01_070758_add_column_to_products_table.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('invoice_products', function (Blueprint $table) {
-            $table->id();
-            $table->string('ProductName');
-            $table->string('ProductNumber');
-            $table->timestamps();
+        Schema::table('invoice_products', function (Blueprint $table) {
+            $table->string('Description')->nullable();
         });
     }
 
@@ -24,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('invoice_products');
+        Schema::table('invoice_products', function (Blueprint $table) {
+            //
+        });
     }
 };

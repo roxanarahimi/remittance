d5cf747712ffb8d76215b5eb1c6485f3ae227ab8
Roxana <ms.roxanarahimi@gmail.com>
2024-07-01 10:59:35 +0330

    test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -33,7 +33,7 @@ class TestController extends Controller
 //
 //             return Invoice::all();
 //            (new CacheController)->cacheProducts();
-            return InvoiceProduct::select('id','ProductName as Name','ProductNumber','Description')->get();
+//            return InvoiceProduct::select('id','ProductName as Name','ProductNumber','Description')->get();
 
                 $data = Test::orderByDesc('id')->get();
             return response(TestResource::collection($data), 200);

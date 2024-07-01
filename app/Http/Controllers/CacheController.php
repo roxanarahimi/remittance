b2046c2b16c51e4ae5bcc7c462164563b33b37d6
Roxana <ms.roxanarahimi@gmail.com>
2024-07-01 10:47:02 +0330

    test

--- app/Http/Controllers/CacheController.php
+++ app/Http/Controllers/CacheController.php
@@ -17,6 +17,7 @@ class CacheController extends Controller
 {
     public function cacheProducts()
     {
+        InvoiceProduct::query()->truncate();
         $productnumbers = InvoiceProduct:://        where('CreationDate', '>=', today()->subDays(2))->
         pluck('ProductNumber');
         $products = Product::where('Name', 'like', '%نودالیت%')->whereNotIn('Number', $productnumbers)->get();

d6b0e6b8fa5a059abdec47e3cfedbcc3517d6ad7
Roxana <ms.roxanarahimi@gmail.com>
2024-07-01 11:04:11 +0330

    test

--- app/Http/Resources/TestResource.php
+++ app/Http/Resources/TestResource.php
@@ -15,6 +15,7 @@ class TestResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            "id"=>$this->id,
             "invoice_id" => $this->invoice_id,
             "Barcode" => $this->Barcode,
             "OrderID" => $this->invoice?->OrderID,

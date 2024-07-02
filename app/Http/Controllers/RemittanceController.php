b6abfbaf5c455269d1c2797d277a8b624db24c60
Roxana <ms.roxanarahimi@gmail.com>
2024-07-02 17:58:12 +0330

    test

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -214,7 +214,7 @@ class RemittanceController extends Controller
             ->join('LGS3.Plant', 'LGS3.Plant.PlantID', '=', 'LGS3.Store.PlantRef')
             ->join('GNR3.Address', 'GNR3.Address.AddressID', '=', 'LGS3.Plant.AddressRef')
             ->where('LGS3.InventoryVoucher.FiscalYearRef', 1403)
-            ->where('LGS3.InventoryVoucher.Date', '>=', today()->subDays(7))
+//            ->where('LGS3.InventoryVoucher.Date', '>=', today()->subDays(7))
 //            ->whereIn('LGS3.Store.StoreID', $storeIDs)
             ->whereIn('LGS3.InventoryVoucher.InventoryVoucherSpecificationRef', [69])
 //            ->where(function ($q) use ($storeIDs) {

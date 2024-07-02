a3d70bed53f5bf4a8fbf5dcee549e942dcb8089e
Roxana <ms.roxanarahimi@gmail.com>
2024-07-02 18:09:44 +0330

    test

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -209,14 +209,14 @@ class RemittanceController extends Controller
             ->pluck('StoreID');
         $dat = InventoryVoucher::select("LGS3.InventoryVoucher.InventoryVoucherID", "LGS3.InventoryVoucher.Number",
             "LGS3.InventoryVoucher.CreationDate", "Date as DeliveryDate", "CounterpartStoreRef",
-        "LGS3.InventoryVoucher.InventoryVoucherSpecificationRef")
-            ->join('LGS3.Store', 'LGS3.Store.StoreID', '=', 'LGS3.InventoryVoucher.CounterpartStoreRef')
+        "LGS3.InventoryVoucher.InventoryVoucherSpecificationRef","DelivererOrReceiverPartyRef")
+            ->join('DNR3.Party', 'DNR3.Party.PartyID', '=', 'LGS3.InventoryVoucher.CounterpartStoreRef')
             ->join('LGS3.Plant', 'LGS3.Plant.PlantID', '=', 'LGS3.Store.PlantRef')
             ->join('GNR3.Address', 'GNR3.Address.AddressID', '=', 'LGS3.Plant.AddressRef')
             ->where('LGS3.InventoryVoucher.FiscalYearRef', 1403)
 //            ->where('LGS3.InventoryVoucher.Date', '>=', today()->subDays(7))
 //            ->whereIn('LGS3.Store.StoreID', $storeIDs)
-            ->whereIn('LGS3.InventoryVoucher.InventoryVoucherSpecificationRef', [69])
+//            ->whereIn('LGS3.InventoryVoucher.InventoryVoucherSpecificationRef', [68])
 //            ->where(function ($q) use ($storeIDs) {
 //                $q->whereIn('LGS3.InventoryVoucher.InventoryVoucherSpecificationRef', [69])
 //                    ->orwhere(function ($z) use ($storeIDs) {

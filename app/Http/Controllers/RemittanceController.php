117a5270d89108635c2f2c08389c1375c7118fec
Roxana <ms.roxanarahimi@gmail.com>
2024-07-02 18:52:14 +0330

    test

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -195,6 +195,7 @@ class RemittanceController extends Controller
 
     public function readOnly1(Request $request)
     {
+        return  DB::connection('sqlsrv')->table('GNR3.Party')->get(100);
         $dat = InventoryVoucher::select("*")
             ->join('GNR3.Party', 'GNR3.Party.PartyID', '=', 'LGS3.InventoryVoucher.CounterpartEntityRef')
             ->join('GNR3.Address', 'GNR3.Address.AddressID', '=', 'LGS3.Party.AddressRef')

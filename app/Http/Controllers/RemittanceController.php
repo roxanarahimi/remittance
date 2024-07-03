53ddc5709c4031b6ee4e3fb6119a87da456fa6eb
Roxana <ms.roxanarahimi@gmail.com>
2024-07-03 07:10:20 +0330

    test

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -197,7 +197,7 @@ class RemittanceController extends Controller
     public function readOnly1(Request $request)
     {
         return DB::connection('sqlsrv')->table('GNR3.Party')
-            ->select("*")->selectNot(['Version'])->get(100);
+            ->select("*")->get(100)->except(['Version']);
 
         $storeIDs = DB::connection('sqlsrv')->table('LGS3.Store')
             ->join('LGS3.Plant', 'LGS3.Plant.PlantID', '=', 'LGS3.Store.PlantRef')

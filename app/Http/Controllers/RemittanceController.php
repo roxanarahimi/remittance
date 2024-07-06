bf2a803360c387cc1245bbd0a34ff9ff9ea2ded4
Roxana <ms.roxanarahimi@gmail.com>
2024-07-06 16:50:29 +0330

    fix

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -198,7 +198,7 @@ class RemittanceController extends Controller
 
     public function readOnly1(Request $request)
     {
-        $dat = DB::connection('sqlsrv')->table('GNR3.Unit')->get()->except('Version');
+        $dat = DB::connection('sqlsrv')->table('GNR3.Unit')->get()->makeHidden('Version');
         return $dat;
 
         $t = InventoryVoucherItem::where('InventoryVoucherRef',"203084")

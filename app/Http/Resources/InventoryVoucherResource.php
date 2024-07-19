d915fd950d5852f1238105de6479e0b6e325f8ed
Roxana <ms.roxanarahimi@gmail.com>
2024-07-19 06:02:00 +0330

    query

--- app/Http/Resources/InventoryVoucherResource.php
+++ app/Http/Resources/InventoryVoucherResource.php
@@ -24,10 +24,10 @@ class InventoryVoucherResource extends JsonResource
 //            "AddressName" => $this->Store?->Name . $this->CounterpartEntityText . ' ' .$this->Number,
 //            "Address" => $this->Store->Plant->Address->Details,
 //            "Phone" => $this->Store->Plant->Address->Phone,
-            "AddressID" => $this->Store->Plant?->Address->AddressID. $this->Store->Party?->Address->AddressID,
+            "AddressID" => $this->Store->Plant?->Address->AddressID. $this->Store->Party?->PartyAddress->AddressID,
             "AddressName" => $this->Store?->Name . $this->CounterpartEntityText . ' ' .$this->Number,
-            "Address" => $this->Store->Plant?->Address->Details. $this->Store->Party?->Address->Details,
-            "Phone" => $this->Store->Plant?->Address->Phone. $this->Store->Party?->Address->Phone,
+            "Address" => $this->Store->Plant?->Address->Details. $this->Store->Party?->PartyAddress->Details,
+            "Phone" => $this->Store->Plant?->Address->Phone. $this->Store->Party?->PartyAddress->Phone,
 
             "Type" => "InventoryVoucher",
             'Sum' => $this->OrderItems->sum('Quantity'),

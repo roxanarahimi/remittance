d22fae8d312d2c33c406fe413fcbae3264a8a3b3
Roxana <ms.roxanarahimi@gmail.com>
2024-08-11 15:42:46 +0330

    11

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -632,7 +632,7 @@ class RemittanceController extends Controller
         }
         $dat3 = Address::select('GNR3.Address.AddressID', 'GNR3.Address.Name as AddressName', 'GNR3.RegionalDivision.Name as City')
             ->join('GNR3.RegionalDivision', 'GNR3.RegionalDivision.RegionalDivisionID', '=', 'GNR3.Address.RegionalDivisionRef')
-            ->where('AddressID', '192300')->get();
+           ->get();
         return $dat3;
     }
 

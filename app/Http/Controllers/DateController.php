609940b20a6b18bf9ba9c10c671c5c4f3ed85e87
Roxana <ms.roxanarahimi@gmail.com>
2025-01-08 16:39:56 +0330

    aa

---
+++ app/Http/Controllers/DateController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class DateController extends Controller
+{
+    public function toPersian($date)
+    {
+        date_default_timezone_set('Asia/Tehran');
+        $formatter=new \IntlDateFormatter(
+            'fa-IR@calender=persian',
+            \IntlDateFormatter::FULL,
+            \IntlDateFormatter::FULL,
+            'Asia/Tehran',
+            \IntlDateFormatter::TRADITIONAL,
+            "yyyy-MM-d H:m:s"
+        );
+
+
+        $dateTime = \datetime::createfromformat('Y-m-d H:i:s',$date);
+        return $formatter->format($dateTime);
+
+    }
+}

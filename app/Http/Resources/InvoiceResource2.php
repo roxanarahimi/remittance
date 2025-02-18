159f5dee4dc9a95ead476665e0fc3763ddf9b69f
Roxana <ms.roxanarahimi@gmail.com>
2025-02-18 18:24:50 +0330

    aa

--- app/Http/Resources/InvoiceResource2.php
+++ app/Http/Resources/InvoiceResource2.php
@@ -44,7 +44,7 @@ class InvoiceResource2 extends JsonResource
             'count' => $this->invoiceItems->sum('Quantity'),
 
             "DeliveryDate" => $this->DeliveryDate,
-            'DeliveryDate22' => (new DateController)->toPersian(explode(' ',$this->DeliveryDate)[0]),
+            'DeliveryDate22' => (new DateController)->toPersian($this->DeliveryDate),
             'created_at' => explode(' ',(new DateController)->toPersian($this->created_at))[0].' '.explode(' ',(new DateController)->toPersian($this->created_at))[1]
 
 

aeee0cfa5eb6bd3d422e89410fb1469491109665
Roxana <ms.roxanarahimi@gmail.com>
2025-02-18 12:43:23 +0330

    aa

--- app/Http/Resources/InvoiceResource.php
+++ app/Http/Resources/InvoiceResource.php
@@ -59,7 +59,7 @@ class InvoiceResource extends JsonResource
 //            'DeliveryDate' => explode(' ',(new DateController)->toPersian($this->DeliveryDate))[0].' '.explode(' ',(new DateController)->toPersian($this->DeliveryDate))[1],
 
             "OrderItems" => InvoiceItemResource::collection($this->invoiceItems),
-//            'created_at' => explode(' ',(new DateController)->toPersian($this->created_at))[0].' '.explode(' ',(new DateController)->toPersian($this->created_at))[1]
+            'created_at' => explode(' ',(new DateController)->toPersian($this->created_at))[0].' '.explode(' ',(new DateController)->toPersian($this->created_at))[1]
 
 
         ];

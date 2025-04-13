6d90c8a2b9e5402751f7833943274c3766419d22
Roxana <ms.roxanarahimi@gmail.com>
2025-04-14 02:00:48 +0330

    aa

--- app/Models/TourItem.php
+++ app/Models/TourItem.php
@@ -20,6 +20,6 @@ class TourItem extends Model
     }
     public function CustomerAddress()
     {
-        return $this->belongsTo(CustomerAddress::class,'CustomerAddressID', 'CustomerAddressRef');
+        return $this->hasOne(CustomerAddress::class,'CustomerAddressID', 'CustomerAddressRef');
     }
 }

a749ae84600e70a9c7e85fbdc694ce49b31353d0
Roxana <ms.roxanarahimi@gmail.com>
2025-04-14 10:43:02 +0330

    aa

--- app/Models/TourInvoice.php
+++ app/Models/TourInvoice.php
@@ -16,6 +16,6 @@ class TourInvoice extends Model
     }
     public function Order()
     {
-        return $this->belongsTo(Order::class, 'OrderID', 'OrderRef');
+        return $this->belongsTo(Order::class, 'OrderID', 'OrderRef')->with('OrderItems');
     }
 }

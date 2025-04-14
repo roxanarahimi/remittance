2b701a0de1175568af16bd58b3102d13fa4db028
Roxana <ms.roxanarahimi@gmail.com>
2025-04-14 10:38:06 +0330

    aa

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -621,7 +621,7 @@ class RemittanceController extends Controller
     public function fix(Request $request)
     {
 
-        $dat = Tour::orderBy('TourID')->take(20)->get();
+        $dat = Tour::orderByDESC('TourID')->paginate(50);
         return TourResource::collection($dat);
 
         $dat = DB::connection('sqlsrv')->table('LGS3.Transporter')->select("TransporterID")

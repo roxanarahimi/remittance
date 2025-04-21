e344cf04a743bcc78876d9354fc98d7d2cf99479
Roxana <ms.roxanarahimi@gmail.com>
2025-04-21 10:33:23 +0330

    aa

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -624,7 +624,7 @@ class RemittanceController extends Controller
 
 
 
-        $dat = Transporter::orderByDESC('TourID')->where('TelNumber', $request['mobile'])->first();
+        $dat = Transporter::where('TelNumber', $request['mobile'])->first();
         return $dat;
         $dat = Tour::orderByDESC('TourID')->whereHas('invoices', function ($q) use ($request) {
             $q->whereHas('order',function($d){

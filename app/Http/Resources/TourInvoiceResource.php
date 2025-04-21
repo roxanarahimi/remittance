c53f679163bf1ae85c926f042dc304be87091ecb
Roxana <ms.roxanarahimi@gmail.com>
2025-04-21 13:04:14 +0330

    aa

--- app/Http/Resources/TourInvoiceResource.php
+++ app/Http/Resources/TourInvoiceResource.php
@@ -22,7 +22,7 @@ class TourInvoiceResource extends JsonResource
             "Customer"=> [
                 "CustomerID"=> $this->Order->Customer->CustomerID,
                 "Number"=> $this->Order->Customer->Number,
-                "FullName"=> $this->Order->Customer->Party->FullName,
+                "Name"=> $this->Order->Customer->Party->Name,
                 "NationalID"=> $this->Order->Customer->Party->NationalID,
                 "Mobile"=> $this->Order->Customer->Party->Mobile,
                 "Tel"=> $this->Order->Customer->Party->Tel,

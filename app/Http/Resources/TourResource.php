284d8113a37770dae125a7cc6607b8eb4d3a46a2
Roxana <ms.roxanarahimi@gmail.com>
2025-04-21 15:56:51 +0330

    aa

--- app/Http/Resources/TourResource.php
+++ app/Http/Resources/TourResource.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Resources;
 
-use App\Http\Controllers\DateController;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -15,20 +14,18 @@ class TourResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $this->EndDate!=null? $end= explode(' ', (new DateController)->toPersian($this->EndDate))[0]: $end='';
         return [
-            "TourID" => $this->TourID,
-            "Number" => $this->Number,
-            "StartDate" => explode(' ', (new DateController)->toPersian($this->StartDate))[0],
-
-            "EndDate" => $end,
-            "State" => $this->State,
+"TourID"=>$this->TourID,
+"Number"=>$this->Number,
+"StartDate"=> $this->StartDate,
+"EndDate"=> $this->EndDate,
+"State"=> $this->State,
 //"FiscalYearRef"=> $this->FiscalYearRef,
-            "CreationDate" => explode(' ', (new DateController)->toPersian($this->CreationDate))[0],
+"CreationDate"=> $this->CreationDate,
 //"SentToHandheld"=> $this->SentToHandheld,
 //"Description"=> $this->Description,
 //            "items"=> $this->TourItems,
-            "invoices" => TourInvoiceResource::collection($this->Invoices),
+            "invoices"=> TourInvoiceResource::collection($this->Invoices) ,
 
         ];
     }

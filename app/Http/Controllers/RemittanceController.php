c5367d08742cf39726583a8b209dbd42950ec742
Roxana <ms.roxanarahimi@gmail.com>
2025-04-22 10:18:19 +0330

    aa

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -624,7 +624,7 @@ class RemittanceController extends Controller
         $dat = Tour::orderByDESC('TourID')
             ->where('State',2)
 //            ->whereDate('StartDate',date(today()))
-            ->whereDate('StartDate','>=', today()->subDays(2))
+            ->whereDate('StartDate','>=', today()->subDays(1))
             ->whereHas('invoices', function ($q) use ($request) {
                 $q->whereHas('order',function($d){
                     $d->whereHas('orderItems');

eb29cab45b0efd72a34be5790646e505ca77b7c1
Roxana <ms.roxanarahimi@gmail.com>
2025-04-29 15:49:04 +0330

    aa

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -626,7 +626,7 @@ class RemittanceController extends Controller
 //        $party = Party::orderByDESC('PartyID')->where('Mobile',$request['mobile'])->with('Transporter')->first();
 //        return [$party];
 
-        $t = Transporter::orderByDESC('TransporterID')->where('PartyRef',"259224")->get();
+        $t = Transporter::orderByDESC('TransporterID')->where('PartyRef',"259224")->with('Party')->get();
         return [$t];
         return new PartyResource2($party);
 

94b5353e990a90d1c24315c1c2060b82996f45da
Roxana <ms.roxanarahimi@gmail.com>
2025-04-29 17:07:11 +0330

    aa

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -631,12 +631,6 @@ class RemittanceController extends Controller
         $dat = Tour::orderByDESC('TourID')
             ->where('State', 2)
             ->whereDate('StartDate', date(today()))
-            ->whereHas('invoices', function ($q) use ($request) {
-                $q->whereHas('order', function ($d) {
-                    $d->whereHas('orderItems');
-                });
-
-            })
             ->whereHas('TourAssignmentItem', function ($z) use ($request) {
                 $z->whereHas('Assignment', function ($x) use ($request) {
                     $x->whereHas('Transporter', function ($y) use ($request) {
@@ -644,6 +638,13 @@ class RemittanceController extends Controller
                     });
                 });
             })
+            ->whereHas('invoices', function ($q) use ($request) {
+                $q->whereHas('order', function ($d) {
+                    $d->whereHas('orderItems');
+                });
+
+            })
+
             ->where('FiscalYearRef', 1405)
 //            ->get();
             ->paginate(100);

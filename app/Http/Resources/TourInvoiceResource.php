73ed363ccad0aaa5649dcbb3aec3ce63d3fd5019
Roxana <ms.roxanarahimi@gmail.com>
2025-04-29 21:36:50 +0330

    aa

--- app/Http/Resources/TourInvoiceResource.php
+++ app/Http/Resources/TourInvoiceResource.php
@@ -15,10 +15,10 @@ class TourInvoiceResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            "InvoiceID"=> $this->InvoiceID,
-            "OrderID"=> $this->Order?->OrderID,
-            "OrderNumber"=> $this->Order?->Number,
-            "Amount"=> (integer)$this->Order?->Price,
+            "InvoiceID" => $this->InvoiceID,
+            "OrderID" => $this->Order?->OrderID,
+            "OrderNumber" => $this->Order?->Number,
+            "Amount" => (integer)$this->Order?->Price,
 
 //            "Party ID"=> $this->Tour->TourAssignmentItem?->Assignment?->Transporter?->Party->PartyID,
 //            "Party Ref"=> $this->Tour->TourAssignmentItem?->Assignment?->Transporter?->PartyRef,
@@ -30,18 +30,19 @@ class TourInvoiceResource extends JsonResource
 //            "OrderRef"=> $this->OrderRef,
 //            "Order"=> $this->Order,
 //            "Customer"=> [
-                "CustomerID"=> $this->Order?->Customer->CustomerID,
-                "CustomerNumber"=> $this->Order?->Customer->Number,
-                "FullName"=> $this->Order?->Customer->Party->FullName,
-                "NationalID"=> $this->Order?->Customer->Party->NationalID,
-                "Mobile"=> $this->Order?->Customer->Party->Mobile,
-                "Tel"=> $this->Order?->Customer->Party->Tel,
-                "Phone"=> $this->Order?->Customer->CustomerAddress->Address->Phone,
-                "AddressName"=> $this->Order?->Customer->CustomerAddress->Address->Name,
-                "Region"=> $this->Order?->Customer->CustomerAddress->Address->Region->Name,
-                "Address"=> $this->Order?->Customer->CustomerAddress->Address->Details,
-                "Latitude"=> $this->Order?->Customer->CustomerAddress->Address->Latitude,
-                "Longitude"=> $this->Order?->Customer->CustomerAddress->Address->Longitude,
+            "CustomerID" => $this->Order?->Customer->CustomerID,
+            "CustomerNumber" => $this->Order?->Customer->Number,
+            "FullName" => $this->Order?->Customer->Party->FullName,
+            "Mobile" => $this->Order?->Customer->Party->Mobile,
+            "NationalID" => $this->Order?->Customer->Party->NationalID,
+            "BirthDate" => $this->Order?->Customer->Party->BirthDate,
+            "Tel" => $this->Order?->Customer->Party->Tel,
+            "Phone" => $this->Order?->Customer->CustomerAddress->Address->Phone,
+            "AddressName" => $this->Order?->Customer->CustomerAddress->Address->Name,
+            "Region" => $this->Order?->Customer->CustomerAddress->Address->Region->Name,
+            "Address" => $this->Order?->Customer->CustomerAddress->Address->Details,
+            "Latitude" => $this->Order?->Customer->CustomerAddress->Address->Latitude,
+            "Longitude" => $this->Order?->Customer->CustomerAddress->Address->Longitude,
 //            ],
 
         ];

366b7340003a52a3b9dd31b7c3ef8cba10c87406
Roxana <ms.roxanarahimi@gmail.com>
2025-04-29 14:11:46 +0330

    aa

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -631,7 +631,7 @@ class RemittanceController extends Controller
             $q->with('TourAssignmentItem', function ($z) use ($request) {
                 $z->with('Assignment', function ($x) use ($request) {
                     $x->with('Transporter', function ($y) use ($request) {
-                        $y->with('Party', function ($p) use ($request) {
+                        $y->WhereHas('Party', function ($p) use ($request) {
                             $p->where('Mobile',$request['mobile']);
                         });
                     });

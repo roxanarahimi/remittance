d3ab35517e9348e32e974f07cfe4b04d2048e139
Roxana <ms.roxanarahimi@gmail.com>
2025-04-29 19:44:02 +0330

    aa

--- app/Models/Party.php
+++ app/Models/Party.php
@@ -22,7 +22,7 @@ class Party extends Model
     }
     public function Transporters()
     {
-        return $this->hasMany(Transporter::class,'PartyRef','PartyID');
+        return $this->hasMany(Transporter::class,'PartyID','PartyRef');
 //            ->whereHas('Assignments',function ($q){
 //                $q->whereHas('TourAssignmentItem',function ($x){
 //                    $x->whereHas('Tour');

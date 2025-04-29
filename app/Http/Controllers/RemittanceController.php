241bf485ea4cc715d6bd56e16c57a86467825320
Roxana <ms.roxanarahimi@gmail.com>
2025-04-29 20:03:12 +0330

    aa

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -623,13 +623,13 @@ class RemittanceController extends Controller
 
     public function fix(Request $request)
     {
-        $party = Party::orderByDESC('PartyID')->where('Mobile',$request['mobile'])
-            ->where('NationalID',"!=","")
-            ->first();
-        $ts = Transporter::orderByDESC('TransporterID')->where('PartyRef',$party['PartyID'])->get();
-//
-//        return $party;
-        return [new PartyResource2($party),$ts];
+//        $party = Party::orderByDESC('PartyID')->where('Mobile',$request['mobile'])
+//            ->where('NationalID',"!=","")
+//            ->first();
+//        $ts = Transporter::orderByDESC('TransporterID')->where('PartyRef',$party['PartyID'])->get();
+////
+////        return $party;
+//        return [new PartyResource2($party),$ts];
 
         $dat = Tour::orderByDESC('TourID')
             ->where('State', 2)

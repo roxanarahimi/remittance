1a6e8eef8ff1d732c5b111333f8d48255b0c06ab
Roxana <ms.roxanarahimi@gmail.com>
2025-05-05 17:41:56 +0330

    commit

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -632,7 +632,7 @@ class RemittanceController extends Controller
 
 //        return $os;
 //        return count($os);
-        $oss = Remittance::select('OrderNumber','orderID')->whereIn('OrderNumber',$os)->get()->unique();
+        $oss = Remittance::select('OrderNumber','orderID')->whereIn('OrderNumber',$os)->get()->toArray()->unique();
         return $oss;
         $is = Invoice::WhereIn('OrderNumber',$os)->get();
                 return [count($os),count($is)];

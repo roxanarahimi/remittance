52916223cf8b700ebd199529f6a1b538febc4bcf
Roxana <ms.roxanarahimi@gmail.com>
2025-05-11 16:25:38 +0330

    commit

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -632,7 +632,7 @@ class RemittanceController extends Controller
         $os = DB::table('remittances')
             ->select('OrderNumber','barcode')
             ->where('OrderNumber',137)
-            ->where('barcode','701031835101800000058A22004000003652')
+//            ->where('barcode','701031835101800000058A22004000003652')
             ->get();
         return count($os);
 

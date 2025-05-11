5b1829f040f7681fd8ddd0c1dcfa5b97e8e689be
Roxana <ms.roxanarahimi@gmail.com>
2025-05-11 17:38:09 +0330

    commit

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -638,7 +638,9 @@ class RemittanceController extends Controller
                     ->on('invoices.Type', '=', 'dupes.Type');
             })
             ->select('invoices.*') // includes 'id' and all other columns
-            ->with('barcodes','rrBarcodes')->get();
+            ->with('barcodes')
+            ->with('rrBarcodes')
+            ->get();
         return $duplicates;
 
         $duplicates = DB::table('invoices')

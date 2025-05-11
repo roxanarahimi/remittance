cfc1d1fc5056a824a73b52249709daccb2135f7b
Roxana <ms.roxanarahimi@gmail.com>
2025-05-11 20:38:22 +0330

    commit

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -639,7 +639,7 @@ class RemittanceController extends Controller
 //        return $duplicates;
 
         $d = Invoice::whereIn('id', $duplicates)
-            ->orderByDesc('OrderID')
+            ->orderBy('OrderID')
             ->with('barcodes')
             ->with('rrBarcodes')
             ->paginate(500);

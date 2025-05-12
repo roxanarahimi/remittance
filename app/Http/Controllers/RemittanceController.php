d72b42454d8ef26c610c1374af0708c6ce5c3195
Roxana <49621427+roxanarahimi@users.noreply.github.com>
2025-05-12 18:02:43 +0330

    Update RemittanceController.php
    sd

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -626,11 +626,11 @@ class RemittanceController extends Controller
 //            ->get();
 //        return [$duplicates, count($duplicates)];
         $d = Invoice::select('*')
-//            ->whereIn('OrderID', $duplicates)
-            ->where('OrderID', '300410')
+            ->whereIn('OrderID', $duplicates)
+//            ->where('OrderID', '300410')
 //            ->orderBy('OrderID')
 //            ->wherehas('barcodes')
-//            ->whereHas('rrBarcodes')
+            ->whereHas('rrBarcodes')
              ->with('barcodes')
             ->with('rrBarcodes')
             ->paginate(100);

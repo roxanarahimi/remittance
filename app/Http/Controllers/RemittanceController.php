2c574d545330864aba94d1636fba1f61dc634755
Roxana <ms.roxanarahimi@gmail.com>
2025-05-13 12:51:14 +0330

    cc

--- app/Http/Controllers/RemittanceController.php
+++ app/Http/Controllers/RemittanceController.php
@@ -641,11 +641,7 @@ class RemittanceController extends Controller
                 ->whereHas('rrBarcodes')
                 ->with('barcodes')
                 ->get();
-            return $t;
-            if(!$t[1]){
-                return $t;
-            }
-            if ($t[1]->barcodes->count() == 0) {
+            if (count($t) && $t[1]->barcodes->count() == 0) {
                 $t[1]->delete();
             }
         }
